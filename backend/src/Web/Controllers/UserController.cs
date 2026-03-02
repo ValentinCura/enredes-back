76b8a00ceb0d5317209894c4e98a6501441515ab
Valentin Cura <valencuran@gmail.com>
2026-03-02 14:47:37 -0300

    Fix: UserController route

--- backend/src/Web/Controllers/UserController.cs
+++ backend/src/Web/Controllers/UserController.cs
@@ -7,11 +7,11 @@ namespace Web.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class UsersController : ControllerBase
+public class UserController : ControllerBase
 {
     private readonly IUserService _userService;
 
-    public UsersController(IUserService userService)
+    public UserController(IUserService userService)
     {
         _userService = userService;
     }
