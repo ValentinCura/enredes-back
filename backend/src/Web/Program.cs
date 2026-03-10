333528d57b1269e0288b8d84b9c2c701163f8238
Valentin Cura <valencuran@gmail.com>
2026-03-10 14:39:41 -0300

    Fix in conection to front

--- backend/src/Web/Program.cs
+++ backend/src/Web/Program.cs
@@ -79,7 +79,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("FrontendPolicy", policy =>
     {
-        policy.WithOrigins("http://localhost:3000", "http://localhost:5173")
+        policy.WithOrigins("http://localhost:3000", "https://localhost:5173")
               .AllowAnyHeader()
               .AllowAnyMethod()
               .AllowCredentials();
